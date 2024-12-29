be3137a34b37be95d2aa64d5305db36753862478
RononDex <tinoheuberger@protonmail.com>
2024-12-29 17:30:20 +0100

    Map whole profile structure to traces

--- connector.go
+++ connector.go
@@ -2,11 +2,14 @@ package profilestotraces
 
 import (
 	"context"
+	"crypto/rand"
+	"strconv"
 
 	"go.uber.org/zap"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pprofile"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
@@ -41,23 +44,39 @@ func (c *connectorImp) Capabilities() consumer.Capabilities {
 func (c *connectorImp) ConsumeProfiles(ctx context.Context, profiles pprofile.Profiles) error {
 	// loop through the levels of spans of the one trace consumed
 	traces := ptrace.NewTraces()
+	traceId := createNewTraceId()
+
 	for i := 0; i < profiles.ResourceProfiles().Len(); i++ {
 		resourceProfile := profiles.ResourceProfiles().At(i)
 		newResourceTrace := traces.ResourceSpans().AppendEmpty()
 
 		for j := 0; j < resourceProfile.ScopeProfiles().Len(); j++ {
 			scopeProfile := resourceProfile.ScopeProfiles().At(j)
+			scopeSpans := newResourceTrace.ScopeSpans().AppendEmpty()
+			scopeProfile.Scope().Attributes().CopyTo(scopeSpans.Scope().Attributes())
 
 			for k := 0; k < scopeProfile.Profiles().Len(); k++ {
 				profile := scopeProfile.Profiles().At(k)
+				profileSpan := scopeSpans.Spans().AppendEmpty()
+				profileSpan.SetTraceID(traceId)
+				profileSpan.SetName("Profile_" + profile.ProfileID().String())
+				profileSpan.SetSpanID(createNewSpanId())
+				profileSpan.SetKind(ptrace.SpanKindInternal)
+				profile.Attributes().CopyTo(profileSpan.Attributes())
+				setProfileAttributes(profile, profileSpan)
 
 				for l := 0; l < profile.Sample().Len(); l++ {
 					sample := profile.Sample().At(l)
-					newTraceSpan := newResourceTrace.ScopeSpans().AppendEmpty()
+					sampleSpan := scopeSpans.Spans().AppendEmpty()
+					sampleSpan.SetTraceID(traceId)
+					sampleSpan.SetSpanID(createNewSpanId())
+					sampleSpan.SetName("Sample")
+					sampleSpan.SetKind(ptrace.SpanKindInternal)
+					sampleSpan.SetParentSpanID(profileSpan.SpanID())
 
-					copyAttributes(sample, profile, newTraceSpan)
+					copyAttributes(sample.AttributeIndices(), profile.AttributeTable(), sampleSpan)
+					copyLocations(sample, profile, profileSpan, scopeSpans, traceId)
 				}
-
 			}
 		}
 	}
@@ -65,13 +84,82 @@ func (c *connectorImp) ConsumeProfiles(ctx context.Context, profiles pprofile.Pr
 	return c.tracesConsumer.ConsumeTraces(ctx, traces)
 }
 
-func copyAttributes(sample pprofile.Sample, profile pprofile.Profile, newTraceSpan ptrace.ScopeSpans) {
-	for m := 0; m < sample.AttributeIndices().Len(); m++ {
-		attributeTableEntry := profile.AttributeTable().At(int(sample.AttributeIndices().At(m)))
-		if attributeTableEntry.Key() == "process.executable.path" {
-			newTraceSpan.Scope().SetName(attributeTableEntry.Value().Str())
+func setProfileAttributes(profile pprofile.Profile, profileSpan ptrace.Span) {
+	profileSpan.Attributes().PutStr("profile.time", profile.Time().String())
+	profileSpan.Attributes().PutInt("profile.period", profile.Period())
+	profileSpan.Attributes().PutStr("profile.duration", profile.Duration().String())
+	profileSpan.Attributes().PutStr("profile.startTime", profile.StartTime().String())
+	profileSpan.Attributes().PutStr("profile.periodType", profile.StringTable().At(int(profile.PeriodType().TypeStrindex())))
+
+	for i := 0; i < profile.CommentStrindices().Len(); i++ {
+		commentIdx := profile.CommentStrindices().At(i)
+		comment := profile.StringTable().At(int(commentIdx))
+		profileSpan.Attributes().PutStr("Comment"+strconv.Itoa(i+1), comment)
+	}
+}
+
+func copyLocations(sample pprofile.Sample, profile pprofile.Profile, profileSpan ptrace.Span, scopeSpans ptrace.ScopeSpans, traceId pcommon.TraceID) {
+	locationIdxOffset := sample.LocationsStartIndex()
+	parentSpanId := profileSpan.SpanID()
+	for locationIdx := sample.LocationsLength(); locationIdx >= 0; locationIdx-- {
+		location := profile.LocationTable().At(int(locationIdx) + int(locationIdxOffset))
+
+		locationSpan := scopeSpans.Spans().AppendEmpty()
+		locationSpan.SetTraceID(traceId)
+		locationSpan.SetSpanID(createNewSpanId())
+		locationSpan.SetParentSpanID(parentSpanId)
+		locationSpan.SetName("Location")
+
+		if location.Line().Len() > 0 {
+			line := location.Line().At(0)
+			functionName := profile.StringTable().At(int(profile.FunctionTable().At(int(line.FunctionIndex())).NameStrindex()))
+
+			locationSpan.SetName(functionName)
+			locationSpan.Attributes().PutInt("location.lineNr", line.Line())
+			locationSpan.Attributes().PutInt("location.columnNr", line.Column())
+			locationSpan.Attributes().PutStr("location.functionName", functionName)
 		}
-		newAttribute := newTraceSpan.Scope().Attributes().PutEmpty(attributeTableEntry.Key())
+		if location.HasMappingIndex() {
+			mappingIdx := location.MappingIndex()
+			mapping := profile.MappingTable().At(int(mappingIdx))
+
+			if mapping.HasFilenames() {
+				locationSpan.Attributes().PutStr("mapping.fileName", profile.StringTable().At(int(mapping.FilenameStrindex())))
+			}
+		}
+
+		copyAttributes(location.AttributeIndices(), profile.AttributeTable(), locationSpan)
+
+		parentSpanId = locationSpan.SpanID()
+	}
+}
+
+func copyAttributes(attributeIndices pcommon.Int32Slice, attributeTable pprofile.AttributeTableSlice, targetSpan ptrace.Span) {
+	for m := 0; m < attributeIndices.Len(); m++ {
+		attributeTableEntry := attributeTable.At(int(attributeIndices.At(m)))
+		newAttribute := targetSpan.Attributes().PutEmpty(attributeTableEntry.Key())
 		attributeTableEntry.Value().CopyTo(newAttribute)
 	}
+}
+
+func createNewTraceId() pcommon.TraceID {
+	traceIdBytes := make([]byte, 16)
+	_, err := rand.Read(traceIdBytes)
+	if err != nil {
+		panic("Error while creating new trace ID: " + err.Error())
+	}
+	traceId := pcommon.TraceID(traceIdBytes)
+
+	return traceId
+}
+
+func createNewSpanId() pcommon.SpanID {
+	spanIdBytes := make([]byte, 8)
+	_, err := rand.Read(spanIdBytes)
+	if err != nil {
+		panic("Error while creating new span ID: " + err.Error())
+	}
+	spanId := pcommon.SpanID(spanIdBytes)
+
+	return spanId
 }
--- go.mod
+++ go.mod
@@ -12,10 +12,12 @@ require (
 	go.opentelemetry.io/collector/pdata/pprofile v0.115.0
 	go.opentelemetry.io/otel v1.32.0 // indirect
 	go.uber.org/zap v1.27.0
+	github.com/google/uuid v1.6.0
 )
 
 require (
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
